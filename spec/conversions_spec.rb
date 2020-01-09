6b8a0f712157ceab025ecf1db74463b65aa511a6
Jelson Rodriguez <jelson.rodriguez1012@gmail.com>
2020-01-09 22:48:25 +0000

    Automatically backed up by Learn

--- spec/conversions_spec.rb
+++ spec/conversions_spec.rb
@@ -8,7 +8,7 @@ describe "conversions" do
     end
     
     it "given 1, it should return 28.3495"
-      answer = ounces_to_grams(1)
+      grams = ounces_to_grams(1)
 if answer == 28.3495
   puts "Passed!"
 else
