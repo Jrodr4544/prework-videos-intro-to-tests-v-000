e59dffe66aff527618af63ef1526e912826947e5
Jelson Rodriguez <jelson.rodriguez1012@gmail.com>
2020-01-09 22:48:18 +0000

    Automatically backed up by Learn

--- spec/conversions_spec.rb
+++ spec/conversions_spec.rb
@@ -7,6 +7,12 @@ describe "conversions" do
       expect(grams).to eq(0.0)
     end
     
-    it ''
+    it "given 1, it should return 28.3495"
+      answer = ounces_to_grams(1)
+if answer == 28.3495
+  puts "Passed!"
+else
+  puts "Test failed: got #{answer} instead"
+end
   end
 end
