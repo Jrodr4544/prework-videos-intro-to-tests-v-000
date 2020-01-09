13fa84ddff6faf9e789b1e8a230840ccb29d3d90
Jelson Rodriguez <jelson.rodriguez1012@gmail.com>
2020-01-09 22:50:32 +0000

    Automatically backed up by Learn

--- spec/conversions_spec.rb
+++ spec/conversions_spec.rb
@@ -7,7 +7,7 @@ describe "conversions" do
       expect(grams).to eq(0.0)
     end
     
-    it "given 1, returns 28.3495"
+    it "given 1, returns 28.3495" do
       grams = ounces_to_grams(1)
       expect(grams).to eq(28.3495)
     end
